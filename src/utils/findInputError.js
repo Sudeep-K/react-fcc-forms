eba2ff96e4524c5b29bdc761378bba5062691d84
Sudeep-K <pong32btl@gmail.com>
2023-11-06 23:40:22 +0545

    completed✅

--- src/utils/findInputError.js
+++ src/utils/findInputError.js
@@ -14,5 +14,6 @@ export function findInputError(errors, name) {
     .reduce((cur, key) => {
       return Object.assign(cur, { error: errors[key] })
     }, {})
+    console.log("filtered", filtered)
   return filtered
 }
